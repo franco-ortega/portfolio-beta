1546b54c84f03db4d8b6e6e5d3af9ad1ed50ef52
Franco Ortega <francopdx@gmail.com>
2024-07-30 16:37:58 -0700

    create reusable fixture components for h2 through h6 tags

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -1,4 +1,9 @@
 import Heading1 from '@/components/fixtures/headings/Heading1';
+import Heading2 from '@/components/fixtures/headings/Heading2';
+import Heading3 from '@/components/fixtures/headings/Heading3';
+import Heading4 from '@/components/fixtures/headings/Heading4';
+import Heading5 from '@/components/fixtures/headings/Heading5';
+import Heading6 from '@/components/fixtures/headings/Heading6';
 
 export default function FixturesPage() {
 	return (
@@ -9,6 +14,11 @@ export default function FixturesPage() {
 			<main>
 				<section>
 					<Heading1 heading={'Heading 1'} />
+					<Heading2 heading={'Heading 2'} />
+					<Heading3 heading={'Heading 3'} />
+					<Heading4 heading={'Heading 4'} />
+					<Heading5 heading={'Heading 5'} />
+					<Heading6 heading={'Heading 6'} />
 				</section>
 			</main>
 		</div>
---
+++ components/fixtures/headings/Heading2.tsx
@@ -0,0 +1,5 @@
+import { HeadingProps } from '@/types/fixtures';
+
+export default function Heading2({ heading }: HeadingProps) {
+	return <h2>{heading}</h2>;
+}
