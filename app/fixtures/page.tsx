c2da10645154f8d8c00ea49e807e7ac0c9eb47ee
Franco Ortega <francopdx@gmail.com>
2024-07-30 16:23:28 -0700

    create Fixtures page skeleton

---
+++ app/fixtures/page.tsx
@@ -0,0 +1,14 @@
+export default function FixturesPage() {
+	return (
+		<div>
+			<header>
+				<h1>Fixtures</h1>
+			</header>
+			<main>
+				<section>
+					<h2>Section 1</h2>
+				</section>
+			</main>
+		</div>
+	);
+}
