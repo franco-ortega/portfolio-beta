c3c75a9adfb0d7beee886cc3d3dc538bcaa5214a
Franco Ortega <francopdx@gmail.com>
2024-07-30 16:34:21 -0700

    create types file for fixture types

--- components/fixtures/headings/Heading1.tsx
+++ components/fixtures/headings/Heading1.tsx
@@ -1,6 +1,4 @@
-type HeadingProps = {
-	heading: string;
-};
+import { HeadingProps } from '@/types/fixtures';
 
 export default function Heading1({ heading }: HeadingProps) {
 	return <h1>{heading}</h1>;
