3c4711b846bbd5108ca7ef492a1f8123832a7f5b
Franco Ortega <francopdx@gmail.com>
2024-07-31 16:07:48 -0700

    create custom properties for heading color and font family

--- components/fixtures/headings/Heading6.tsx
+++ components/fixtures/headings/Heading6.tsx
@@ -1,5 +1,6 @@
 import { HeadingProps } from '@/types/fixtures';
+import styles from './Heading.module.css';
 
 export default function Heading6({ heading }: HeadingProps) {
-	return <h6>{heading}</h6>;
+	return <h6 className={styles.Heading}>{heading}</h6>;
 }
