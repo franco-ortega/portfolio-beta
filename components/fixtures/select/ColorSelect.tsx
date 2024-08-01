1af2c017ba8015a455cd3ffddc2e702de041cee1
Franco Ortega <francopdx@gmail.com>
2024-08-01 16:24:23 -0700

    add label and move color data into a list

--- components/fixtures/select/ColorSelect.tsx
+++ components/fixtures/select/ColorSelect.tsx
@@ -5,26 +5,29 @@ type Props = {
 	color: string;
 };
 
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
 export default function ColorSelect({ handler, color }: Props) {
 	function onColorSelect(e: React.ChangeEvent<HTMLSelectElement>) {
 		handler(e.target.value);
 	}
 
 	return (
-		<select
-			name='color-picker'
-			id='color-picker'
-			onChange={onColorSelect}
-			value={color}
-		>
-			<option value=''>Pick Color</option>
-			<option value='red'>red</option>
-			<option value='orange'>orange</option>
-			<option value='yellow'>yellow</option>
-			<option value='green'>green</option>
-			<option value='blue'>blue</option>
-			<option value='indigo'>indigo</option>
-			<option value='violet'>violet</option>
-		</select>
+		<>
+			<label htmlFor='color-picker'>Pick Color: </label>
+			<select
+				name='select'
+				id='color-picker'
+				onChange={onColorSelect}
+				value={color}
+			>
+				<option value=''></option>
+				{colors.map((color) => (
+					<option key={color} value={color}>
+						{color}
+					</option>
+				))}
+			</select>
+		</>
 	);
 }
