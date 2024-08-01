09e0ba50e57bf9589ea28a2bd0274aa5f85c0482
Franco Ortega <francopdx@gmail.com>
2024-08-01 16:03:51 -0700

    remove deprecated heading components

--- components/fixtures/headings/Heading3.tsx
+++
@@ -1,6 +0,0 @@
-import { HeadingProps } from '@/types/fixtures';
-import styles from './Heading.module.css';
-
-export default function Heading3({ heading }: HeadingProps) {
-	return <h3 className={styles.Heading}>{heading}</h3>;
-}
--- components/fixtures/headings/Heading4.tsx
+++
@@ -1,6 +0,0 @@
-import { HeadingProps } from '@/types/fixtures';
-import styles from './Heading.module.css';
-
-export default function Heading4({ heading }: HeadingProps) {
-	return <h4 className={styles.Heading}>{heading}</h4>;
-}
