f6d795934d675e0ad353ea23e0320f2bf8e90152
Franco Ortega <francopdx@gmail.com>
2024-08-02 10:34:57 -0700

    create Content fixture with mutliple amounts of lorem: small, medium, large

---
+++ components/fixtures/content/Content.tsx
@@ -0,0 +1,26 @@
+const contentSmall =
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione mollitia eum praesentium, fuga optio aut accusamus corporis aliquid corrupti dolor eaque ipsum saepe. Maiores, laborum!';
+
+const contentMedium =
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, dicta aperiam dolor voluptatibus corrupti at non hic in totam nisi quidem, neque autem cumque praesentium consectetur quibusdam error! Quidem, quia praesentium in nulla quas ut reprehenderit, culpa facere ab, eligendi fuga ipsam. Vero sint recusandae voluptates assumenda rem nemo!';
+
+const contentLarge =
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. A neque debitis voluptatum magnam iusto velit deleniti consectetur, modi adipisci ea mollitia ab dignissimos, quidem ipsum impedit, ipsa nisi esse illum distinctio possimus quasi maiores. Quo suscipit sit nemo laudantium ad sapiente voluptatibus consectetur ipsa, nihil obcaecati placeat ea sed nobis corrupti iure illum in expedita? Illo autem dolore distinctio dignissimos perspiciatis recusandae quae reprehenderit quod vitae! Placeat, beatae eum id quidem assumenda voluptas necessitatibus laborum.';
+
+const contents = {
+	small:
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione mollitia eum praesentium, fuga optio aut accusamus corporis aliquid corrupti dolor eaque ipsum saepe. Maiores, laborum!',
+	medium:
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ad, dicta aperiam dolor voluptatibus corrupti at non hic in totam nisi quidem, neque autem cumque praesentium consectetur quibusdam error! Quidem, quia praesentium in nulla quas ut reprehenderit, culpa facere ab, eligendi fuga ipsam. Vero sint recusandae voluptates assumenda rem nemo!',
+	large:
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. A neque debitis voluptatum magnam iusto velit deleniti consectetur, modi adipisci ea mollitia ab dignissimos, quidem ipsum impedit, ipsa nisi esse illum distinctio possimus quasi maiores. Quo suscipit sit nemo laudantium ad sapiente voluptatibus consectetur ipsa, nihil obcaecati placeat ea sed nobis corrupti iure illum in expedita? Illo autem dolore distinctio dignissimos perspiciatis recusandae quae reprehenderit quod vitae! Placeat, beatae eum id quidem assumenda voluptas necessitatibus laborum.',
+};
+
+type Props = {
+	size: keyof typeof contents;
+};
+
+export default function Content({ size }: Props) {
+	const currentContent = contents[size];
+	return <p>{currentContent}</p>;
+}
