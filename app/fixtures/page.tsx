97bab8888697d21077063abee4fa7ddcac3656f7
Franco Ortega <francopdx@gmail.com>
2024-08-08 17:31:25 -0700

    add options 4 and 5

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -42,6 +42,8 @@ export default function FixturesPage() {
 						<option value={'option1'}>Option 1</option>
 						<option value={'option2'}>Option 2</option>
 						<option value={'option3'}>Option 3</option>
+						<option value={'option4'}>Option 4</option>
+						<option value={'option5'}>Option 5</option>
 					</CustomSelect>
 				</div>
 			</header>
