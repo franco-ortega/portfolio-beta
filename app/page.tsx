e382f2ac6137d302acaacbb143a2141fa6d7d044
Franco Ortega <francopdx@gmail.com>
2024-08-16 15:42:13 -0700

    create ga ga

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import Content from '@/components/fixtures/content/Content';
 import styles from './page.module.css';
+import Heading from '@/components/fixtures/headings/Heading';
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<h1>Hello, Portfolio</h1>
-		</main>
+		<>
+			<header>
+				<Heading
+					content='Hello'
+					heading='h1'
+					headingStyles={{
+						color: 'var(--green)',
+					}}
+				/>
+			</header>
+			<main className={styles.main}>
+				<section>
+					<Content size={'small'} />
+					<Content size={'medium'} />
+					<Content size={'large'} />
+				</section>
+			</main>
+		</>
 	);
 }
