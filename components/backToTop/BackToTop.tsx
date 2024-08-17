15bb52dd3d25e806c2e08444f643d0bbe7426703
Franco Ortega <francopdx@gmail.com>
2024-08-16 17:13:53 -0700

    move BackToTop component into separate file

---
+++ components/backToTop/BackToTop.tsx
@@ -0,0 +1,9 @@
+import styles from './BackToTop.module.css';
+
+export default function BackToTop() {
+	return (
+		<div className={styles.BackToTop}>
+			<a href='#top'>Back to Top</a>
+		</div>
+	);
+}
