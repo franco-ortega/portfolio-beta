b5920627725088a7bf8d1f00f9d074a2840b782a
Franco Ortega <francopdx@gmail.com>
2024-08-16 17:52:14 -0700

    create enum to handle string props passed from BackToTop to JumpLink

--- components/backToTop/BackToTop.tsx
+++ components/backToTop/BackToTop.tsx
@@ -1,10 +1,15 @@
 import JumpLink from '../links/jumpLink/JumpLink';
 import styles from './BackToTop.module.css';
 
+const JUMP_LINK = {
+	link: 'top',
+	linkText: 'Back to Top',
+};
+
 export default function BackToTop() {
 	return (
 		<div className={styles.BackToTop}>
-			<JumpLink link='top' linkText='Back to Top' />
+			<JumpLink link={JUMP_LINK.link} linkText={JUMP_LINK.linkText} />
 		</div>
 	);
 }
